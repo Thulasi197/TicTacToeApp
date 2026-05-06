34eb49745c27c91538214a99f1061f02eeaee41d
thulasi <thulasi195v@gmail.com>
2026-05-06 09:26:00 +0530

    UC7 implemented

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
-class BoardUpdater {
-    public void updateBoard(char[][] board, int row, int col, char symbol) {
-        board[row][col] = symbol;
-        System.out.println("Board updated successfully!");
-    }
-    public void displayBoard(char[][] board) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " | ");
+import java.util.Random;
+
+class ComputerMove {
+    public void makeMove(char[][] board, char symbol) {
+
+        Random random = new Random();
+        int row, col;
+        while (true) {
+            int slot = random.nextInt(9) + 1;
+            row = (slot - 1) / 3;
+            col = (slot - 1) % 3;
+            if (board[row][col] == ' ') {
+                board[row][col] = symbol; // Place symbol
+                System.out.println("Computer chose slot: " + slot);
+                break;
             }
-            System.out.println();
         }
     }
 }
@@ -19,12 +24,13 @@ public class Main {
             {' ', ' ', ' '},
             {' ', ' ', ' '}
         };
-        BoardUpdater updater = new BoardUpdater();
-        int row = 1;
-        int col = 1;
-        char symbol = 'X';
-        updater.updateBoard(board, row, col, symbol);
-
-        updater.displayBoard(board);
+        ComputerMove computer = new ComputerMove();
+        computer.makeMove(board, 'O');
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+        }
     }
 }
