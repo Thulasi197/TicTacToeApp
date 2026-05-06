5c1728b695887a0e0d518654c7723ad7c46202d1
thulasi <thulasi195v@gmail.com>
2026-05-06 09:52:05 +0530

    UC9 implemented

--- Main.java
+++ Main.java
@@ -1,96 +1,46 @@
-import java.util.Random;
-
+class WinChecker {
+    public boolean checkWin(char[][] board, char symbol) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol &&
+                board[i][1] == symbol &&
+                board[i][2] == symbol) {
+                return true;
+            }
+        }
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == symbol &&
+                board[1][j] == symbol &&
+                board[2][j] == symbol) {
+                return true;
+            }
+        }
+        if (board[0][0] == symbol &&
+            board[1][1] == symbol &&
+            board[2][2] == symbol) {
+            return true;
+        }
+        if (board[0][2] == symbol &&
+            board[1][1] == symbol &&
+            board[2][0] == symbol) {
+            return true;
+        }
+        return false;
+    }
+}
 public class Main {
-
-    static char[][] board = {
-        {' ', ' ', ' '},
-        {' ', ' ', ' '},
-        {' ', ' ', ' '}
-    };
-
-    static char currentPlayer = 'X';
-
     public static void main(String[] args) {
 
-        boolean gameOver = false;
+        char[][] board = {
+            {'X', 'X', 'X'},
+            {'O', ' ', 'O'},
+            {' ', ' ', ' '}
+        };
+        WinChecker checker = new WinChecker();
 
-        while (!gameOver) {
-
-            // Computer makes a move (for demo, both players auto-play)
-            makeRandomMove(currentPlayer);
-
-            displayBoard();
-
-            // Check win
-            if (checkWin(currentPlayer)) {
-                System.out.println("Player " + currentPlayer + " wins!");
-                gameOver = true;
-            }
-            // Check draw
-            else if (isBoardFull()) {
-                System.out.println("It's a draw!");
-                gameOver = true;
-            }
-            // Switch turn
-            else {
-                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-            }
+        if (checker.checkWin(board, 'X')) {
+            System.out.println("Player X wins!");
+        } else {
+            System.out.println("No winner yet.");
         }
-    }
-
-    // UC7 logic reused – random valid move
-    static void makeRandomMove(char symbol) {
-        Random random = new Random();
-        int row, col;
-
-        while (true) {
-            int slot = random.nextInt(9) + 1;
-
-            row = (slot - 1) / 3;
-            col = (slot - 1) % 3;
-
-            if (board[row][col] == ' ') {
-                board[row][col] = symbol;
-                System.out.println("Player " + symbol + " chose slot: " + slot);
-                break;
-            }
-        }
-    }
-
-    // Display board
-    static void displayBoard() {
-        System.out.println();
-        for (int i = 0; i < 3; i++) {
-            System.out.println(board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
-        }
-        System.out.println();
-    }
-
-    // Check win condition
-    static boolean checkWin(char s) {
-
-        // Rows, Columns, Diagonals
-        return (board[0][0] == s && board[0][1] == s && board[0][2] == s) ||
-               (board[1][0] == s && board[1][1] == s && board[1][2] == s) ||
-               (board[2][0] == s && board[2][1] == s && board[2][2] == s) ||
-
-               (board[0][0] == s && board[1][0] == s && board[2][0] == s) ||
-               (board[0][1] == s && board[1][1] == s && board[2][1] == s) ||
-               (board[0][2] == s && board[1][2] == s && board[2][2] == s) ||
-
-               (board[0][0] == s && board[1][1] == s && board[2][2] == s) ||
-               (board[0][2] == s && board[1][1] == s && board[2][0] == s);
-    }
-
-    // Check draw condition
-    static boolean isBoardFull() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == ' ') {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
